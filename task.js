9fe08382beedbeb492d4165655c83072b48e2bc9
SmolenskiyAV <JoJo_666@list.ru>
2021-10-09 18:27:06 +0700

    Версия 1.0

--- task.js
+++ task.js
@@ -1,7 +1,9 @@
 function compareArrays(arr1, arr2) {
-  let result;
+  let result = false; // дефолтное значение результата до начала проверок
 
-  // Ваш код
+  if (arr1.length !== arr2.length)  return result; // если размеры массивов разные, прекращаем проверку и выводим false
+
+  if (arr1.every((n, i) => n === arr2[i])) result = true;
 
   return result; // boolean
 }
@@ -9,7 +11,10 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr
+  .filter((element) => element > 0) // оставляем токмо эдементы строго больше нуля
+  .filter((element) => element % 3 === 0) // оставляем токмо кратные трём
+  .map((element) => element * 10); // оставшиеся элементы умножаем на 10 и формируем из них массив
 
   return resultArr; // array
 }
